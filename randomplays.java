1f14f4abaa883c7080b862c8b2e05ab6caba72dc
meupceoco <120145305+meupceoco@users.noreply.github.com>
2025-01-19 23:28:44 -0300

    Delete randomplays.java

--- randomplays.java
+++
@@ -1,9 +0,0 @@
-import banco.Conector;
-
-public class randomplays {
-    public static void main(String[] args) {
-        Conector c = new Conector();
-
-        System.out.println("sou mais louco que todos vocês");
-    }
-}
