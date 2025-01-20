571b7438b44f5c1f50ad2df704aa090888cea7c2
meupceoco <120145305+meupceoco@users.noreply.github.com>
2025-01-19 23:28:08 -0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+import view.UsuarioMenu;
+
+public class main {
+    public static void main(String[] args) {
+        UsuarioMenu usuarioMenu = new UsuarioMenu();
+        usuarioMenu.menuPrincipal(args);
+    }
+    
+}
---
+++ randomplays.java
@@ -0,0 +1,9 @@
+import banco.Conector;
+
+public class randomplays {
+    public static void main(String[] args) {
+        Conector c = new Conector();
+
+        System.out.println("sou mais louco que todos vocês");
+    }
+}
